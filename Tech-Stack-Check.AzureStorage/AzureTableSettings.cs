9aa2f4b6eb1e94c24ba388bdb3dc3677f29f8aa8
Shawn Lam <shawn@coderfrontline.com>
2018-08-19 13:51:02 +1200

    Add AzureStorage abstraction layer

---
+++ Tech-Stack-Check.AzureStorage/AzureTableSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TechStackCheck.AzureStorage.Table
+{
+    public class AzureTableSettings
+    {
+        public AzureTableSettings(string storageAccountConnectionString)
+        {
+            if (string.IsNullOrEmpty(storageAccountConnectionString))
+                throw new ArgumentNullException(nameof(storageAccountConnectionString));
+
+            this.StorageAccountConnectionString = storageAccountConnectionString;
+        }
+
+        public string StorageAccountConnectionString { get; }
+    }
+}
