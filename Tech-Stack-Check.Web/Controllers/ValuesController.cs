b3c86a9ccbce95cfaaf8c16297152fe372c83ca3
Shawn Lam <shawn@coderfrontline.com>
2018-08-19 14:51:11 +1200

    Setup NLog as preferred logging provider with sample configuration. Minor cleanup of configuration files.

--- Tech-Stack-Check.Web/Controllers/ValuesController.cs
+++ Tech-Stack-Check.Web/Controllers/ValuesController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TechStackCheck.Web.Services;
 
 namespace TechStackCheck.Web.Controllers
@@ -12,15 +11,19 @@ namespace TechStackCheck.Web.Controllers
     public class ValuesController : ControllerBase
     {
         private readonly IValuesService valuesService;
+        private readonly ILogger<ValuesController> logger;
 
-        public ValuesController(IValuesService valuesService){
+        public ValuesController(IValuesService valuesService, ILogger<ValuesController> logger)
+        {
             this.valuesService = valuesService;
+            this.logger = logger;
         }
 
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            logger.LogInformation("Get");
             return new string[] { "value1", "value2" };
         }
 
@@ -28,6 +31,7 @@ namespace TechStackCheck.Web.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
+            logger.LogInformation($"Get {id}");
             return Ok(await valuesService.GetValue(id));
         }
 
@@ -35,18 +39,21 @@ namespace TechStackCheck.Web.Controllers
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            logger.LogInformation("Post");
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            logger.LogInformation("Post");
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            logger.LogInformation("Delete");
         }
     }
 }
